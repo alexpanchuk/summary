9b2a7f8e4a452e5ffc2a754855e7c107e0ab4913
Alex Panchuk <apanchuk@gmail.com>
2017-11-20 16:03:44 +0500

    add server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log("Server is running")
+import app from "./app"
+
+const PORT = process.env.PORT || 4000
+
+const server = app.listen(PORT, err => {
+  if (err) console.log(err)
+
+  console.log(`Server running on port: ${PORT}`)
+})
+
+export default server
