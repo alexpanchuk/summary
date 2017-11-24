690b33d63f882e6c4615b7893c489c903529a87e
Alex Panchuk <apanchuk@gmail.com>
2017-11-25 01:33:33 +0500

    add handler for 404 in UserController.getSummariesByUserHash

--- src/modules/users/controllers/users-controller.js
+++ src/modules/users/controllers/users-controller.js
@@ -14,6 +14,10 @@ export default {
 
     const summaries = await Summary.find({ userHash })
 
+    if (!summaries) {
+      ctx.throw(404, `Summaries of user with hash "${userHash}" not found"`)
+    }
+
     ctx.body = { data: summaries }
   }
 }
