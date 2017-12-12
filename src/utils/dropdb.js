f6cf5a6c28f43efb2070399a502fc0976f0c87bb
Alex Panchuk <apanchuk@gmail.com>
2017-12-12 22:58:01 +0500

    remove error handling in dropdb.js

--- src/utils/dropdb.js
+++ src/utils/dropdb.js
@@ -3,15 +3,9 @@ import mongooseConnector from "../connectors/mongoose-connector"
 
 async function dropDb() {
   const mongoConnection = await mongooseConnector(MONGO_URI)
-  await mongoConnection.dropDatabase(error => {
-    if (error) {
-      console.log(error)
-    }
-
-    console.log("Database was dropped succesfully")
-
-    mongoConnection.close()
-  })
+  await mongoConnection.dropDatabase()
+  console.log("Mongo dropped")
+  mongoConnection.close()
 }
 
 dropDb()
